59c2e41a0dab7bd0f976b55b5dd8a32504529954
Torgbe-Desmond <123torgbe@gmail.com>
2026-01-04 03:38:42 +0000

    BUG: fixed wrong import

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
 import { Routes, Route } from "react-router-dom";
-import { useEffect } from "react";
-
 import Header from "./Header";
 import Home from "./pages/Home";
 import About from "./pages/About";
+import "./App.css";
+import ContributeLayout from "./layout/ContributeLayout";
+import ExploreLayout from "./layout/ExploreLayout";
 import Assessment from "./pages/Assessment";
 import Stories from "./pages/Stories";
 import SingleResponse from "./pages/SingleResponse";
 import Contact from "./pages/Contact";
+import { useEffect } from "react";
 import Login from "./pages/Login";
-
-import ExploreLayout from "./layout/ExploreLayout";
-import ContributeLayout from "./layout/ContributeLayout";
-import ProtectedRoute from "./components/ProtectedRoute";
-
-import "./App.css";
 
 export default function App() {
   useEffect(() => {
@@ -23,29 +19,17 @@ export default function App() {
       themeColorMeta.setAttribute("content", "#ffffff");
     }
   }, []);
-
   return (
     <>
       <Header />
-
       <main className="container">
         <Routes>
-          {/* 🔓 Public Routes */}
           <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/about" element={<About />} />
+          <Route path="/explore" element={<ExploreLayout />} />
           <Route path="/feedback" element={<Contact />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/assessment" element={<Assessment />} />
           <Route path="/response/:responseId" element={<SingleResponse />} />
-
-          {/* 🔒 Protected Routes */}
-          <Route element={<ProtectedRoute />}>
-            <Route path="/assessment" element={<Assessment />} />
-            <Route path="/explore" element={<ExploreLayout />} />
-            <Route path="/stories" element={<Stories />} />
-            <Route path="/contribute" element={<ContributeLayout />} />
-          </Route>
-
-          {/* ❌ 404 fallback (optional) */}
           <Route path="*" element={<Home />} />
         </Routes>
       </main>
